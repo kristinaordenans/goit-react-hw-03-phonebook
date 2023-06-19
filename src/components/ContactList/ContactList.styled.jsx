3443ad3a64c1e7b17b3b830cd22548b93a361f10
kristinaordenans <okristin92@gmail.com>
2023-06-19 21:45:30 +0300

    localStorage

---
+++ src/components/ContactList/ContactList.styled.jsx
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const ContactListContainer = styled.ul`
+  displey: flex;
+  width: 300px;
+  list-style: none;
+  padding:0;
+  margin-bottom:5px;
+`;
